904cffd1d540d0fff595ec9194fd5c6b02d5d69d
Antoine Augusti <antoine@augusti.fr>
2012-07-29 12:42:46 +0200

    Contact page

---
+++ contact.php
@@ -0,0 +1,19 @@
+<?php
+include 'header.php';
+
+echo '<h1>Contact</h1>';
+
+echo '
+<div id="intro_like">
+	<div class="img_chat fade"></div>
+	<h2>Stay in touch with us</h2>
+	We are very concerned by you, our visitors. You are our top priority and this is the reason why we will be very happy to hear from you!<br/>
+	<br />
+	You have an idea? A suggestion? Something seems to be broken on the website? You want to show us your love? You want to drink a beer with us?<br/>
+	<br/>
+	We will be very happy to answer to your message. Here are a few informations if you which to contact us, by hook or by crook (but we do not accept carrier pigeons).
+</div>';
+
+
+include 'footer.php';
+?>
