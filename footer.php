5f36fc19cf4c5abb1a4c77568186aa6fde9dd041
Antoine Augusti <antoine@augusti.fr>
2012-07-29 11:42:26 +0200

    Close the MySQL connection in the footer

--- footer.php
+++ footer.php
@@ -20,5 +20,6 @@
 	</div><!-- END FOOTER -->
 	<script src="/js/script.min.js"></script>
 	<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+	<?php mysql_close(); ?>
 </body>
 </html>
