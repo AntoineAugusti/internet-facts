fb0e80243f65b2acfcfd188f453616bf7785aaea
Antoine Augusti <antoine@augusti.fr>
2012-08-06 00:04:54 +0200

    Contact page

--- contact.php
+++ contact.php
@@ -4,7 +4,7 @@ include 'header.php';
 echo '<h1>Contact</h1>';
 
 echo '
-<div id="intro_like">
+<div id="intro_like" class="contact_intro">
 	<div class="img_chat fade"></div>
 	<h2>Stay in touch with us</h2>
 	We are very concerned by you, our visitors. You are our top priority and this is the reason why we will be very happy to hear from you!<br/>
@@ -12,6 +12,14 @@ echo '
 	You have an idea? A suggestion? Something seems to be broken on the website? You want to show us your love? You want to drink a beer with us?<br/>
 	<br/>
 	We will be very happy to answer to your message. Here are a few informations if you which to contact us, by hook or by crook (but we do not accept carrier pigeons).
+</div>';
+
+echo '
+<div class="post">
+	<h2>Choose your weapon</h2>
+	<span class="icon_24 email"></span><a href="mailto:contact@pretty-web.com" title="Email us">contact@pretty-web.com</a><br/>
+	<br/>
+	<span class="icon_24 twitter"></span><a href="//twitter.com/'.$twitter_username.'" title="Our Twitter">@'.$twitter_username.'</a><br/>
 </div>';
 
 
