6cd8a6b53aa3a561ae7a02bdd20b2e9aa1e0adc2
Antoine Augusti <antoine@augusti.fr>
2012-08-08 12:00:18 +0200

    New introduction for index / About page

---
+++ about.php
@@ -0,0 +1,14 @@
+<?php
+include 'header.php';
+
+echo '<h1>About</h1>';
+
+echo '
+<div id="intro">
+	Our life is full of details. Small details that are important. Aren\'t you curious? Want to know all those little details that make the beauty of our lives? You will learn amazing things. You will impress your friends. Give it a try and let\'s read some Facts!<br/>
+	<br />
+	Internet Facts: facts about everything in your everyday life that you don\'t know.<br/>
+	<div class="clear"></div>
+</div>';
+
+include 'footer.php';
--- index.php
+++ index.php
@@ -2,15 +2,11 @@
 include 'header.php';
 
 	// Display a description of the website only in the index. It can be hide thanks to Ajax and a session.
-	if (!isset($_SESSION['hide_intro']) AND $_SESSION['hide_intro'] != TRUE AND empty($_GET['mod']))
+	if (!isset($_SESSION['hide_intro']) AND $_SESSION['hide_intro'] != TRUE AND empty($_GET['mod']) AND empty($_GET['p']))
 	{
 		echo '
 		<div id="intro">
-			Our life is full of details. Small details that are important. Aren\'t you curious? Want to know all those little details that make the beauty of our lives? You will learn amazing things. You will impress your friends. Give it a try and let\'s read some Facts!<br/>
-			<br />
-			Internet Facts: facts about everything in your everyday life that you don\'t know.<br/>
-			<span class="right"><a onclick="hide_intro();return false;" title="Hide the introduction">Okey, I understand. Hide this!</a></span>
-			<div class="clear"></div>
+			Internet Facts: the Facts that you never knew in your life.
 		</div>';
 	}
 
